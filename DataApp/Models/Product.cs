5929729d55380c215dd0cbb2527bf37895402faa
Quang Duong <duongnhatquang18@gmail.com>
2020-03-07 15:09:12 +0700

    add model, controller

---
+++ DataApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
